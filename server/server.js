9bab914b3358360fbabdc93139a75facde04f9c1
Elham Ahmadzadeh <eliahm63@gmail.com>
2021-05-23 10:04:00 +0200

    fixed bug

--- server/server.js
+++ server/server.js
@@ -11,8 +11,6 @@ app.use(express.static(path.join(__dirname, '../client/public')))
 const { fetchedApi } = require('./api')
 let index = 0
 let player = undefined
-let correct = 0
-let incorrect = 0
 let correctAnswer = undefined
 const sendNextQuestion = async function () {
   let data = await fetchedApi()
@@ -28,13 +26,14 @@ const sendNextQuestion = async function () {
     correctAnswer = results[0].correct_answer
     questionAndAnswers.all_answers = shuffle(questionAndAnswers.all_answers)
 
-    io.in('viewerRoom').emit('sendOnlyQuestion', `Question${index+1}: ${questionAndAnswers.question}`)
+    io.in('viewerRoom').emit(
+      'sendOnlyQuestion',
+      `Question${index + 1}: ${questionAndAnswers.question}`
+    )
     io.in('playerRoom').emit('sendQuestion', questionAndAnswers)
-
   } catch (err) {
     console.log(err)
   }
-
 }
 
 io.on('connect', (socket) => {
@@ -43,45 +42,45 @@ io.on('connect', (socket) => {
     socket.join('playerRoom')
     socket.emit('player', 'You are the player!')
     player = socket.id
-
   } else {
     socket.join('viewerRoom')
     socket.emit('player', 'player is connected')
   }
   console.log(`client with id ${socket.id} connected`)
-
   socket.on('checkAnswers', (ans) => {
     index++
+    sendNextQuestion()
     if (ans === correctAnswer) {
       io.in('viewerRoom').emit('correctAnswer', `${ans}(correct) `)
-
     } else {
-      io.in('viewerRoom').emit('correctAnswer', `${ans}(wrong) The correct answer is ${correctAnswer}`)
-      sendNextQuestion()
+      io.in('viewerRoom').emit(
+        'correctAnswer',
+        `${ans}(wrong) The correct answer is ${correctAnswer}`
+      )
+
     }
     if (index === 5) {
       io.in('playerRoom').emit('end', 'Finished!')
       changePlayer(socket)
     }
   })
-
   socket.on('disconnect', () => {
     console.log(`client ${socket.id} disconnected`)
     if (socket.id === player) {
-     disconnectPlayer(socket)
+      disconnectPlayer(socket)
     }
   })
 })
+
 function changePlayer(socket) {
-  socket.to('viewerRoom').emit('playerLeaves' , 'Player is disconnected')
+  socket.to('viewerRoom').emit('playerLeaves', 'Player is disconnected')
   socket.leave('playerRoom')
   index = 0
 }
 function disconnectPlayer(socket) {
   player = undefined
-socket.leave('playerRoom')
-io.in('viewerRoom').emit('playerLeft', 'Player left the room!')
-
+  socket.leave('playerRoom')
+  io.in('viewerRoom').emit('playerLeft', 'Player left the room!')
 }
 server.listen(process.env.PORT || 3000, () =>
   console.log(`Server has started on port 3000.`)
